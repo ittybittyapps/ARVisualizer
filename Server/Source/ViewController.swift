e9aaf3bf16e5b8f9202be0cc803f3d92e3569171
Vlas Voloshin <argentumko@gmail.com>
2018-12-03 16:09:29 +1100

    Extracted “processing” logic out of Server’s view controller.

--- Server/Source/ViewController.swift
+++ Server/Source/ViewController.swift
@@ -6,7 +6,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-final class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, SessionServerDelegate {
+final class ViewController: UIViewController, ARSCNViewDelegate, SessionServerDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
 
@@ -14,16 +14,13 @@ final class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelega
         super.viewDidLoad()
 
         self.server.delegate = self
-
-        // Set the view's delegate
         self.sceneView.delegate = self
-        self.sceneView.session.delegate = self
+
+        self.sceneView.session.delegate = self.processor
 
         // Show statistics such as fps and timing information
         self.sceneView.showsStatistics = true
         self.sceneView.debugOptions = [ ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints ]
-
-        NotificationCenter.default.addObserver(self, selector: #selector(statusBarOrientationDidChange(_:)), name: UIApplication.didChangeStatusBarOrientationNotification, object: nil)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -39,12 +36,14 @@ final class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelega
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
 
+        // Remove the example anchor if it's already added
         if let anchor = self.exampleAnchor {
             self.sceneView.session.remove(anchor: anchor)
             self.exampleAnchor = nil
             return
         }
 
+        // Hit-test a plane location under touch and place an example achor on it
         guard let point = touches.first?.location(in: self.view) else {
             return
         }
@@ -64,83 +63,22 @@ final class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelega
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            return
-        }
-
-        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
-        box.firstMaterial?.diffuse.contents = UIColor.red
-        node.addChildNode(SCNNode(geometry: box))
-    }
-
-    // MARK: - ARSessionDelegate
-
-    func session(_ session: ARSession, didUpdate frame: ARFrame) {
-        self.generalProcessingQueue.async {
-            self.processFrame(frame)
-        }
-    }
-
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        self.generalProcessingQueue.async {
-            for anchor in anchors {
-                self.server.sendMessage(.anchorAdded(Anchor(arAnchor: anchor)))
-            }
-        }
-    }
-
-    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
-        self.generalProcessingQueue.async {
-            for anchor in anchors {
-                self.server.sendMessage(.anchorUpdated(Anchor(arAnchor: anchor)))
-            }
-        }
-    }
-
-    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
-        self.generalProcessingQueue.async {
-            for anchor in anchors {
-                self.server.sendMessage(.anchorRemoved(Anchor(arAnchor: anchor)))
-            }
+        // Render a red box to represent the example anchor
+        if anchor === self.exampleAnchor {
+            let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+            box.firstMaterial?.diffuse.contents = UIColor.red
+            node.addChildNode(SCNNode(geometry: box))
         }
     }
 
     // MARK: - SessionServerDelegate
 
-    func sessionServerDidUpdateConnectedPeers(_ sessionServer: SessionServer) {
-    }
+    func sessionServerDidUpdateConnectedPeers(_ sessionServer: SessionServer) { }
 
     // MARK: - Private
 
     private let server = SessionServer()
-    
-    private let generalProcessingQueue = DispatchQueue(label: "com.ittybittyapps.ARVisualizer.Server.GeneralProcessing", qos: .userInitiated)
-    private let pointCloudProcessingQueue = DispatchQueue(label: "com.ittybittyapps.ARVisualizer.Server.PointCloudProcessing", qos: .userInitiated)
-
-    private var atomicInterfaceOrientation = AtomicValue(UIApplication.shared.statusBarOrientation)
-    private var lastScheduledFeaturePoints: ARPointCloud?
-
-    private func processFrame(_ frame: ARFrame) {
-        let encodedFrame = Frame(arFrame: frame, viewingOrientation: self.atomicInterfaceOrientation.value)
-        self.server.sendMessage(.frame(encodedFrame))
-
-        if let featurePoints = frame.rawFeaturePoints, featurePoints != self.lastScheduledFeaturePoints {
-            self.lastScheduledFeaturePoints = frame.rawFeaturePoints
-
-            self.pointCloudProcessingQueue.async {
-                self.processPointCloud(featurePoints, from: frame)
-            }
-        }
-    }
-
-    private func processPointCloud(_ pointCloud: ARPointCloud, from frame: ARFrame) {
-        self.server.sendMessage(.pointCloud(PointCloud(arPointCloud: pointCloud, samplingColorsUsing: frame)))
-    }
-
-    @objc
-    private func statusBarOrientationDidChange(_ notification: Notification) {
-        self.atomicInterfaceOrientation.value = UIApplication.shared.statusBarOrientation
-    }
+    private lazy var processor = SessionProcessor(server: self.server)
 
 }
 
