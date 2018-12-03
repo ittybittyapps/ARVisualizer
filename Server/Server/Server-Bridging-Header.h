fb1566b6f1550d275ef19c39b4f789f7d9dcd514
Vlas Voloshin <argentumko@gmail.com>
2018-12-03 15:22:02 +1100

    Fixed “duplicate definition” issue.

--- Server/Server/Server-Bridging-Header.h
+++ Server/Server/Server-Bridging-Header.h
@@ -2,8 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Core/Core.h>
-
 #import "IBAARAnchor+ARKit.h"
 #import "IBAARCamera+ARKit.h"
 #import "IBAARFrame+ARKit.h"
